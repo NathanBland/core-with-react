f19fedaedbe8429aef4b01947477f693ef572fc2
Nathan Bland <nathan.bland@gmail.com>
2018-12-12 11:40:06 -0700

    Bootstrapping create-react-app to core with docker

--- server/server.js
+++ server/server.js
@@ -13,9 +13,9 @@ app.use(helmet())
 const router = express.Router()
 
 mongoose.Promise = global.Promise
-mongoose.connect(process.env.dbURL || 'mongodb://core-db:27017/core', {useNewUrlParser: true})
+mongoose.connect(process.env.dbURL || 'mongodb://db:27017/core', {useNewUrlParser: true})
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 const server = require('http').Server(app)
 
 app.use(cookieParser())
@@ -30,7 +30,7 @@ app.use(router)
 app.use('/api', require('./routes'))
 
 app.get('/', (req, res) => {
-  return res.status(200).json({msg: 'Welcome to the api'})
+  return res.status(200).json({msg: 'Welcome to the api, running on' + port})
 })
 
 server.listen(port)
